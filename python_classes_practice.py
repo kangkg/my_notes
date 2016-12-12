6df8d37f4894c7daa454d165da2e3b4c3007382a
Kenneth Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-11 19:26:38 -0600

    Create python_classes_practice.py

---
+++ python_classes_practice.py
@@ -0,0 +1,26 @@
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+
+# your code goes here
+car1 = Vehicle()
+car1.value = 60000
+car1.name = 'Fer'
+car1.color = 'red'
+
+car2 = Vehicle()
+car2.color = 'blue'
+car2.value = 10000
+car2.name = "Jump"
+
+
+# test code
+print(car1.description())
+print(car2.description())
